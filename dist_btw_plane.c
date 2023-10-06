1b12c27035f0e9c6f230231102a0babde4dcf976
RanbirPalSingh <146711902+RanbirPalSingh@users.noreply.github.com>
2023-10-06 22:05:02 +0530

    Code to find dist. b/w 2 points in a plane.
    Program to find the Euclidean distance between two points in a plane.

---
+++ dist_btw_plane.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int x1,y1,x2,y2;
+    printf("Enter the value of x1:\n");
+    scanf("%d",&x1);
+    printf("Enter the value of y1:\n");
+    scanf("%d",&y1);
+    printf("Enter the value of x2:\n");
+    scanf("%d",&x2);
+    printf("Enter the value of y2:\n");
+    scanf("%d",&y2);
+
+    printf("%.2f",sqrt(pow(x2-x1,2)+pow(y2-y1,2)));
+    return 0;
+    }
